307df5c4626dd512d157e9e5089b6e5f9d6cd9e8
Roshan Guragain <twistfreaky@gmail.com>
2023-07-27 23:03:50 +0545

    Completed

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,14 +1,23 @@
 
 function insert(num){
+  let input =document.querySelector('.textview')
+  input.value += num
   }
   
   function equals(){
-    
+    let input =document.querySelector('.textview')
+    let y = eval(input.value)
+    input.value= y;
       
   }
   
   function clean(){
+    let input =document.querySelector('.textview')
+    input.value= ''
   }
   
   function back(){
+    let back=document.querySelector(".textview").value
+    document.querySelector(".textview").value = back.substr(0, back.length - 1);
+   
   }
